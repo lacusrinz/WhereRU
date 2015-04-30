4765bff4506793aa13ec1f12769a334ab2b4f75a
ZhiHao Qian <lacusrinz@gmail.com>
2015-04-30 11:53:28 +0800

    remove afnetworking header

--- WhereRU/WhereRU-Bridging-Header.h
+++ WhereRU/WhereRU-Bridging-Header.h
@@ -16,7 +16,7 @@
 
 #import "SVProgressHUD.h"
 #import "JSONKit.h"
-#import "AFNetworking.h"
+//#import "AFNetworking.h"
 #import "SDWebImage/UIImageView+WebCache.h"
 #import "UIActivityIndicator-for-SDWebImage/UIImageView+UIActivityIndicatorForSDWebImage.h"
 #import "TSMessage.h"
