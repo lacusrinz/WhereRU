9f929dbcca3d5bc5f5361e78c9fdf7859ea3ec6d
ZhiHao Qian <lacusrinz@gmail.com>
2015-06-29 23:30:04 +0800

    [ci skip]init calendarView Framework

---
+++ calendarView/calendarView.h
@@ -0,0 +1,19 @@
+//
+//  calendarView.h
+//  calendarView
+//
+//  Created by 钱志浩 on 15/6/29.
+//  Copyright (c) 2015年 RInz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for calendarView.
+FOUNDATION_EXPORT double calendarViewVersionNumber;
+
+//! Project version string for calendarView.
+FOUNDATION_EXPORT const unsigned char calendarViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <calendarView/PublicHeader.h>
+
+
