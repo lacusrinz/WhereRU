49c846b6d4d5c3949c5a5898b94a94713e610770
ZhiHao Qian <lacusrinz@gmail.com>
2016-01-11 17:55:42 +0800

    update

--- WhereRU/AddNewEventViewController.swift
+++ WhereRU/AddNewEventViewController.swift
@@ -13,11 +13,20 @@ class AddNewEventViewController: UIViewController {
     @IBOutlet weak var startTimeHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var endTimeHeightConstraint: NSLayoutConstraint!
     
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var noteFextView: UITextView!
+    @IBOutlet weak var startTimeLabel: UILabel!
+    @IBOutlet weak var startTimeDatePicker: UIDatePicker!
+    @IBOutlet weak var endTimeLabel: UILabel!
+    @IBOutlet weak var endTimeDatePicker: UIDatePicker!
+    @IBOutlet weak var locationLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.automaticallyAdjustsScrollViewInsets = false
         startTimeHeightConstraint.constant = 0
         endTimeHeightConstraint.constant = 0
+        self.titleTextField.becomeFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
